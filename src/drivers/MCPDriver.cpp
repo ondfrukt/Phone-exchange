92b027ab23718252d01823c9f0da506241582c77
ondfrukt <79996783+ondfrukt@users.noreply.github.com>
2025-08-19 12:36:55 +0200

    Update MCPDriver.cpp

--- src/drivers/MCPDriver.cpp
+++ src/drivers/MCPDriver.cpp
@@ -1,12 +1,10 @@
 #include "MCPDriver.h"
+using cfg::mcp
 
 void MCPDriver::begin() {
   mcpMain.begin_I2C(cfg::mcp::MCP_ADDRESS);
   mcpMT8816.begin_I2C(cfg::mcp::MCP_MT8816_ADDRESS);
   mcpSlic1.begin_I2C(cfg::mcp::MCP_SLIC1_ADDRESS);
   mcpSlic2.begin_I2C(cfg::mcp::MCP_SLIC2_ADDRESS);
-}
+};
 
-void MCPDriver::readHookPin(){
-  //Här låter vi funktionaliteten för att läsa av HOOK-pins ligga och debouncingen av det
-}
