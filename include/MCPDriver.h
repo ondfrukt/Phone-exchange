523ecbc10cd91e0a85f22d64bdeec52fbf6094f3
ondfrukt <79996783+ondfrukt@users.noreply.github.com>
2025-08-19 14:02:53 +0200

    Update MCPDriver.h

--- include/MCPDriver.h
+++ include/MCPDriver.h
@@ -6,6 +6,11 @@
 class MCPDriver {
   public:
     void begin();
+    // Getters till de olika MCP:erna
+    Adafruit_MCP23X17& mainDev()  { return mcpMain;  }
+    Adafruit_MCP23X17& mt8816Dev(){ return mcpMT8816;}
+    Adafruit_MCP23X17& slic1Dev(){ return mcpSlic1; }
+    Adafruit_MCP23X17& slic2Dev(){ return mcpSlic2; }
     void setPin(uint8_t addr, uint8_t pin, bool state);
     void readPin(uint8_t addr, uint8_t pin);
 
